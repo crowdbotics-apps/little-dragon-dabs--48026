4ec3412638bce8f0e38265cf2e8258fdd085e86e
Crowdbotics <team@crowdbotics.com>
2024-05-20 17:09:16 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "little-dragon-dabs-48026",
+  url: "https://little-dragon-dabs-48026.botics.co",
+  api: "https://little-dragon-dabs-48026.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
